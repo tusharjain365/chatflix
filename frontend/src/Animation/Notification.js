f91612d97c7ce04b6467e924196ef74e540d7803
Tushar Jain <tusharjain.752001@gmail.com>
2022-06-30 17:12:27 +0530

    Add files via upload

---
+++ frontend/src/Animation/Notification.js
@@ -0,0 +1,9 @@
+const Notification = ({count})=>{
+    return (
+        <div style={{fontSize:'3px'}}>
+            {count}
+        </div>
+    )
+
+}
+export default Notification;
---
+++ frontend/src/Animation/TypingAnimation.jsx
@@ -0,0 +1,12 @@
+import '../components/style.css';
+
+const TypingAnimation = ()=>{
+    return (
+        <div className="typing">
+            <div className='circle'/>
+            <div className='circle'/>
+            <div className='circle'/>
+        </div>
+    )
+}
+export default TypingAnimation
